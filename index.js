4ce342899d4a6ce88c8654d647490d89209f168a
GASgod1111 <reagan.peter@student.moringaschool.com>
2023-06-19 09:50:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution here!
+// Assuming the initial array of cats
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push('Rapha');
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop('Garfield');
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift('Milo');
+}
+function appendCat(name) {
+  return cats.concat(['Broom']);
+}
+
+function prependCat(name) {
+  return [].concat(cats);
+}
+
